2d0517270d18bb5b4460260f3bcf4870dd5252b9
jbro321 <jaehyung0321@gmail.com>
2021-07-03 00:18:09 +0900

    test2

--- test2/django_test2/django_test2/settings.py
+++ test2/django_test2/django_test2/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
