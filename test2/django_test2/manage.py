64b7d2a9b59c094b8baaa6e5e85aa74e0ecedb82
jbro321 <jaehyung0321@gmail.com>
2021-07-02 20:47:41 +0900

    django_test2

--- test2/django_test2/manage.py
+++ test2/django_test2/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JP_test2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_test2.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
